d86cae1840ecc3de9be211b8d2f658ce2f5c1253
Leonardo Galdino <leonardogaldino721@gmail.com>
2022-12-16 19:28:14 -0300

    versão final

---
+++ src/mail/mail.service.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.MailService = void 0;
+var MailService = /** @class */ (function () {
+    function MailService() {
+    }
+    return MailService;
+}());
+exports.MailService = MailService;
